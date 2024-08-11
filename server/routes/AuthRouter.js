d6e50bab58782674093b501c0e8bf5ab2f1d8f65
Aditya-Garg10 <adityagarg646@gmail.com>
2024-08-11 12:56:09 +0530

    commit 3

--- server/routes/AuthRouter.js
+++ server/routes/AuthRouter.js
@@ -2,9 +2,13 @@ import { Router } from "express";
 import  {SignUp,Login,getUserInfo,updateProfile,addProfileImage,removeProfileImage, logOut}  from '../controllers/AuthController.js'
 import { verifyToken } from "../middleware/AuthMiddleware.js";
 import multer from 'multer'
+import path, { dirname } from 'path'
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname  = dirname(__filename)
 
-const upload = multer({dest: "/uploads/profiles/"})
+const upload = multer({dest : path.join(__dirname,"/tmp/uploads/profiles")})
 
 const router = Router();
 
