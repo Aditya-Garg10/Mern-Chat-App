182988f6baa499eaa2ae1b193e7ca6d2d380e102
Aditya-Garg10 <adityagarg646@gmail.com>
2024-08-12 13:49:34 +0530

    dd

--- server/routes/MessageRoutes.js
+++ server/routes/MessageRoutes.js
@@ -2,9 +2,16 @@ import { Router } from "express";
 import { verifyToken } from "../middleware/AuthMiddleware.js";
 import { getMessages, uploadFile } from "../controllers/MessagesController.js";
 import multer from 'multer'
+import path, { dirname } from 'path'
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname  = dirname(__filename)
+
 
 const messageRoute = Router();
-const upload = multer({dest:"/tmp/uploads/files/"})
+
+const upload = multer({dest : path.join(__dirname,"/tmp/uploads/files/")})
 
 messageRoute.post("/getMessages", verifyToken,getMessages )
 messageRoute.post("/uploadFile", verifyToken,upload.single("file"),uploadFile )
