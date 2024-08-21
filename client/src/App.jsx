b654ab4044b920583949f01e861c395df865e640
Aditya-Garg10 <adityagarg646@gmail.com>
2024-08-21 16:03:48 +0530

    last deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect ,useState} from 'react'
 import { Button } from './components/ui/button'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes, useNavigate } from 'react-router-dom'
 import Auth from './pages/auth'
 import Chat from './pages/chat'
 import Profile from './pages/profile'
@@ -25,7 +25,7 @@ const AuthRoute = ({ children }) => {
 }
 
 const App = () => {
-  
+    const navigate = useNavigate();
     const { userInfo , setUserInfo} = useAppStore()
     const [loading, setloading] = useState(true);
     
@@ -60,6 +60,10 @@ const App = () => {
   if(loading){
     return <div>Loading....</div>
   }
+
+  useEffect(()=>{
+    navigate("/")
+  })
   return (
     <>
       <BrowserRouter>
@@ -70,9 +74,9 @@ const App = () => {
             </AuthRoute>            
           }></Route>
           <Route path='/chat' element={
-          
+          <PrivateRoute>
             <Chat />
-          }>            
+          </PrivateRoute>}>            
           </Route>
           <Route path='/profile' element={
           <PrivateRoute>
